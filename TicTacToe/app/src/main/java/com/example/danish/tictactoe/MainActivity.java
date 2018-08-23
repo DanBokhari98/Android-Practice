8db18efe3460b6430dd00987a9eade0b0e366973
danbokhari98 <danbokhari98@gmail.com>
2018-08-22 23:54:46 -0400

    Created Game Logic And Implemented Winning combination Matrices

--- TicTacToe/app/src/main/java/com/example/danish/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/danish/tictactoe/MainActivity.java
@@ -8,13 +8,42 @@ import android.view.Menu;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    // 0 = x, and 1 = O;
+    int activePlayer = 0;
+    //2 means unplayed;
+    int [] gameState = {2,2,2,2,2,2,2,2,2};
 
-    public void dropIn(View view){
+    int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8} ,{0,4,8}, {2,4,6}};
+
+    public void dropIn(View view) {
         ImageView counter = (ImageView) view;
-        counter.setTranslationY(-1000f);
+        System.out.println(counter.getTag().toString());
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.setImageResource(R.drawable.x);
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(500);
+        if (gameState[tappedCounter] == 2) {
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1000f);
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.o);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.x);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(500);
+
+            for(int [] winningPosition : winningPositions){
+                if(gameState[winningPosition[0]] == gameState[winningPosition[1]]
+                        && gameState[winningPosition[1]] == gameState[winningPosition[2]]
+                        && gameState[winningPosition[0]] != 2){
+                     System.out.println(gameState[winningPosition[0]]);
+
+                     //someone has won:
+
+                }
+            }
+        }
     }
 
     @Override
