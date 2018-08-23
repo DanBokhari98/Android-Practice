ce8763beecd7efabfce12e08f04303636446393f
danbokhari98 <danbokhari98@gmail.com>
2018-08-23 01:09:12 -0400

    Fixed Game Logic, Added Rematch

--- TicTacToe/app/src/main/java/com/example/danish/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/danish/tictactoe/MainActivity.java
@@ -5,14 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.MenuItem;
 import android.view.Menu;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     // 0 = x, and 1 = O;
     int activePlayer = 0;
     //2 means unplayed;
     int [] gameState = {2,2,2,2,2,2,2,2,2};
-
+    boolean gameIsActive = true;
     int[][] winningPositions = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8} ,{0,4,8}, {2,4,6}};
 
     public void dropIn(View view) {
@@ -20,15 +23,15 @@ public class MainActivity extends AppCompatActivity {
         System.out.println(counter.getTag().toString());
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if (gameState[tappedCounter] == 2) {
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
             gameState[tappedCounter] = activePlayer;
 
             counter.setTranslationY(-1000f);
             if (activePlayer == 0) {
-                counter.setImageResource(R.drawable.o);
+                counter.setImageResource(R.drawable.x);
                 activePlayer = 1;
             } else {
-                counter.setImageResource(R.drawable.x);
+                counter.setImageResource(R.drawable.o);
                 activePlayer = 0;
             }
             counter.animate().translationYBy(1000f).rotation(360).setDuration(500);
@@ -37,12 +40,49 @@ public class MainActivity extends AppCompatActivity {
                 if(gameState[winningPosition[0]] == gameState[winningPosition[1]]
                         && gameState[winningPosition[1]] == gameState[winningPosition[2]]
                         && gameState[winningPosition[0]] != 2){
-                     System.out.println(gameState[winningPosition[0]]);
+                    //someone has won:
+                    gameIsActive = false;
+                    String winner = "O";
+                    if(gameState[winningPosition[0]] == 0){
+                         winner = "X";
+                     }
 
-                     //someone has won:
+                    TextView winnerMessage = (TextView)findViewById(R.id.winnerMessage);
+                    winnerMessage.setText(winner + " has won!");
 
+                    LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(View.VISIBLE);
+
+                }else{
+                    boolean gameIsOver = true;
+                    for(int counterState : gameState){
+                        if(counterState == 2){ gameIsOver = false;}
+                    }
+                    if(gameIsOver){
+                        TextView winnerMessage = (TextView)findViewById(R.id.winnerMessage);
+                        winnerMessage.setText("Its a Draw!");
+                        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+                        layout.setVisibility(View.VISIBLE);
+                    }
                 }
             }
+        }
+    }
+
+    public void playAgain(View view){
+        gameIsActive = true;
+        LinearLayout layout = (LinearLayout)findViewById(R.id.playAgainLayout);
+        layout.setVisibility(View.INVISIBLE);
+
+        activePlayer = 0;
+        //2 means unplayed;
+        //Restates gameState
+        for(int i = 0; i < gameState.length; i++){
+            gameState[i] = 2;
+        }
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+        for(int i = 0; i < gridLayout.getChildCount(); i++){
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
         }
     }
 
