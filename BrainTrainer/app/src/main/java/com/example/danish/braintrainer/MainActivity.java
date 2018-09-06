adea70ebfdfdc26dd8f4b6ae4cd9a4deec5ab210
danbokhari98 <danbokhari98@gmail.com>
2018-09-05 23:09:58 -0400

    Worked on UI and layouts

--- BrainTrainer/app/src/main/java/com/example/danish/braintrainer/MainActivity.java
+++ BrainTrainer/app/src/main/java/com/example/danish/braintrainer/MainActivity.java
@@ -2,12 +2,40 @@ package com.example.danish.braintrainer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void checkAnswer(View view){
+
+    }
+
+
+    Button startButton;
+    public void start(View view){
+
+        startButton.setVisibility(View.INVISIBLE);
+        TextView sumTextView = (TextView)findViewById(R.id.sumTextView);
+        Random rand = new Random();
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startButton = (Button) findViewById(R.id.startButton);
     }
 }
